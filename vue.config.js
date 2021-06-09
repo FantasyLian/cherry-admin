9ac0bc69271f1f0e99ddcfebeac1948c6d1c2583
Mr.cat <fantasy.lian@163.com>
2021-06-09 15:24:11 +0800

    配置环境

---
+++ vue.config.js
@@ -0,0 +1,48 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '车厘子交易平台'
+      return args
+    })
+  },
+
+  // 开启Gzip压缩
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }))
+    }
+  },
+
+  devServer: {
+    open: true, // 配置自动启动浏览器
+    overlay: {
+      warning: false,
+      errors: true
+    },
+    proxy: {
+      '/api': {
+        target: process.env.VUE_APP_BASE_API,
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
