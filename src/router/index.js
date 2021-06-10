c4769c3c062d21781d982aee93c3f3403740bb71
Mr.cat <fantasy.lian@163.com>
2021-06-10 10:27:36 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '@/views/layout/Layout'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: Layout,
+    children: [
+      { path: '/home', name: 'home', component: () => import('@/views/home/Home') },
+      { path: '/buyer', name: 'buyer', component: () => import('@/views/order/Buyer') },
+      { path: '/seller', name: 'seller', component: () => import('@/views/order/Seller') },
+      { path: '/balance', name: 'balance', component: () => import('@/views/detail/Balance') },
+      { path: '/deposit', name: 'deposit', component: () => import('@/views/detail/Deposit') },
+      { path: '/user', name: 'user', component: () => import('@/views/user/User') },
+      { path: '/404', component: () => import('@/views/NotFound') }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '@/views/login/Login')
+  },
+  {
+    path: '*',
+    redirect: { path: '/404' }
   }
 ]
 
