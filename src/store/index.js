50a0f5e81f9fd80384e5a1ad87dc6da2956778c4
Mr.cat <fantasy.lian@163.com>
2021-06-10 10:28:13 +0800

    增加状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isCollapse: false
   },
   mutations: {
+    SWITCH_COLLAPSE (state) {
+      state.isCollapse = !state.isCollapse
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+
+  plugins: [createPersistedState({
+    storage: window.sessionStorage
+  })]
 })
