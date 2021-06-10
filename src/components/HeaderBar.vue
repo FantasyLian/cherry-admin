7383b2003ba86d837b4674870da343db261a2fb3
Mr.cat <fantasy.lian@163.com>
2021-06-10 16:38:18 +0800

    增加面包屑导航

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,26 +1,77 @@
 <template>
-  <a-icon
-    class="trigger"
-    :type="isCollapse ? 'menu-unfold' : 'menu-fold'"
-    @click="toggleMenu"
-  />
+  <div class="header-wrap">
+    <div class="breadcrumb-wrap">
+      <a-icon
+        class="trigger"
+        :type="isCollapse ? 'menu-unfold' : 'menu-fold'"
+        @click="toggleMenu"
+      />
+      <a-breadcrumb separator="/">
+        <a-breadcrumb-item v-for="(item, index) of $route.matched" :key="index">
+          <router-link :to="item.path">{{ item.meta.title }}</router-link>
+        </a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <a-dropdown>
+      <a class="ant-dropdown-link" @click="(e) => e.preventDefault()">
+        管理员 <a-icon type="down" />
+      </a>
+      <a-menu slot="overlay" @click="handleCommand">
+        <a-menu-item key="1"> 修改登录密码 </a-menu-item>
+        <a-menu-item key="2"> 退出登录 </a-menu-item>
+      </a-menu>
+    </a-dropdown>
+  </div>
 </template>
 <script>
-import { Icon } from 'ant-design-vue'
+import { Icon, Breadcrumb, Dropdown, Menu } from 'ant-design-vue'
 export default {
   components: {
-    AIcon: Icon
+    AIcon: Icon,
+    ABreadcrumb: Breadcrumb,
+    ABreadcrumbItem: Breadcrumb.Item,
+    ADropdown: Dropdown,
+    AMenu: Menu,
+    AMenuItem: Menu.Item
   },
   data () {
     return {
       isCollapse: false
     }
   },
+  mounted () {
+    console.log(this.$route.matched)
+  },
   methods: {
+    // 切换隐藏显示左侧导航栏
     toggleMenu () {
       this.isCollapse = !this.isCollapse
       this.$store.commit('SWITCH_COLLAPSE')
+    },
+    // 点击下拉菜单选项
+    handleCommand ({ key }) {
+      switch (key) {
+        case '2':
+          this.$router.replace({ path: '/login' })
+          // localStorage.removeItem('token')
+          break
+      }
     }
   }
 }
 </script>
+<style lang="less" scoped>
+.header-wrap {
+  padding: 0 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .breadcrumb-wrap {
+    display: flex;
+    align-items: center;
+    .ant-breadcrumb {
+      margin-left: 16px;
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -12,23 +12,64 @@ const routes = [
   {
     path: '/',
     component: Layout,
-    children: [
-      { path: '/', redirect: { name: 'home' } },
-      { path: '/home', name: 'home', component: () => import('@/views/home/Home') },
-      { path: '/buyer', name: 'buyer', component: () => import('@/views/order/Buyer') },
-      { path: '/seller', name: 'seller', component: () => import('@/views/order/Seller') },
-      { path: '/balance', name: 'balance', component: () => import('@/views/detail/Balance') },
-      { path: '/deposit', name: 'deposit', component: () => import('@/views/detail/Deposit') },
-      { path: '/user', name: 'user', component: () => import('@/views/user/User') },
-      { path: '/404', component: () => import('@/views/NotFound') }
-    ]
-  },
-  {
+    meta: { title: '首页' },
+    redirect: '/home',
+    children: [{
+      path: '/home',
+      name: 'home',
+      component: () => import('@/views/home/Home')
+    }]
+  }, {
+    path: '/',
+    component: Layout,
+    meta: { title: '订单' },
+    redirect: '/order/buyer',
+    children: [{
+      path: '/buyer',
+      name: 'buyer',
+      meta: { title: '买家订单' },
+      component: () => import('@/views/order/Buyer')
+    }, {
+      path: '/seller',
+      name: 'seller',
+      meta: { title: '卖家订单' },
+      component: () => import('@/views/order/Seller')
+    }]
+  }, {
+    path: '/',
+    component: Layout,
+    meta: { title: '明细' },
+    redirect: '/detail/balance',
+    children: [{
+      path: '/balance',
+      name: 'balance',
+      meta: { title: '余额明细' },
+      component: () => import('@/views/detail/Balance')
+    },
+    {
+      path: '/deposit',
+      name: 'deposit',
+      meta: { title: '提现明细' },
+      component: () => import('@/views/detail/Deposit')
+    }]
+  }, {
+    path: '/user',
+    component: Layout,
+    meta: { title: '用户' },
+    redirect: '/user',
+    children: [{
+      path: '/user',
+      name: 'user',
+      component: () => import('@/views/user/User')
+    }]
+  }, {
+    path: '/404',
+    component: () => import(/* webpackChunkName: "404" */ '@/views/NotFound')
+  }, {
     path: '/login',
     name: 'Login',
     component: () => import(/* webpackChunkName: "login" */ '@/views/login/Login')
-  },
-  {
+  }, {
     path: '*',
     redirect: { path: '/404' }
   }
